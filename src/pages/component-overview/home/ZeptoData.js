c0245eecb9543d240f2e50e74db777a41bafd213
MarufaKompass <Marufa@hellokompass.com>
2024-10-16 17:51:12 +0600

    add dropdown

--- src/pages/component-overview/home/ZeptoData.js
+++ src/pages/component-overview/home/ZeptoData.js
@@ -32,6 +32,15 @@ export default function ZeptoData() {
         <Search></Search>
       </div>
      </div>
+     <div class="w-32">
+  <label for="options" class="block text-sm font-medium text-gray-700">Select an option</label>
+  <select id="options" class="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-none shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm">
+    <option value="option1">Option 1</option>
+    <option value="option2">Option 2</option>
+    <option value="option3">Option 3</option>
+    <option value="option4">Option 4</option>
+  </select>
+</div>
      <div className="mt-24">
      <div className="grid grid-cols-4 gap-4">
         {zeptoData.map((dataList) => (
